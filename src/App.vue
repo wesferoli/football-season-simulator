8c746c8f5df9564c52ecc1a9af231f3efb666e51
wesferoli <wesleyfer.oli97@gmail.com>
2023-11-09 22:47:25 -0300

    feat: add vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
-<script setup lang="ts"></script>
-
 <template>
-  <main><h1>Hello World!</h1></main>
+  <main>
+    <h1>Hello World!</h1>
+    <v-icon icon="fas fa-home" />
+    <v-btn prepend-icon="$vuetify" variant="outlined"> Button </v-btn>
+  </main>
 </template>
