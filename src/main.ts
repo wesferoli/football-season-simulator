2d5475e1bc6c22630c4794fc12cb39c4e45b412e
wesferoli <wesleyfer.oli97@gmail.com>
2023-11-18 21:46:17 -0300

    feat: add new icons

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,12 @@ import AppVue from './App.vue'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faFutbolBall } from '@fortawesome/free-solid-svg-icons'
+import {
+  faFutbolBall,
+  faArrowCircleRight,
+  faArrowCircleLeft
+} from '@fortawesome/free-solid-svg-icons'
 
-library.add(faFutbolBall)
+library.add(faFutbolBall, faArrowCircleLeft, faArrowCircleRight)
 
 createApp(AppVue).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
