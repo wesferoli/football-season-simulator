5837f80c97883e1e5f222c3cc16e3e4c9f4e881f
wesferoli <wesleyfer.oli97@gmail.com>
2023-12-13 21:55:51 -0300

    feat: added vuex to the project

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import '@/index.scss'
-import { createApp } from 'vue'
 import AppVue from '@/App.vue'
+import { createApp } from 'vue'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
@@ -9,7 +9,13 @@ import {
   faArrowCircleRight,
   faArrowCircleLeft
 } from '@fortawesome/free-solid-svg-icons'
+import { key, store } from './vuex/store'
 
 library.add(faFutbolBall, faArrowCircleLeft, faArrowCircleRight)
 
-createApp(AppVue).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
+const app = createApp(AppVue)
+
+app.component('font-awesome-icon', FontAwesomeIcon)
+app.use(store, key)
+
+app.mount('#app')
